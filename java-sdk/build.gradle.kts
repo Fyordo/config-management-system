312655962c621b7e52e808cc05914460ba32623f
Fedor <lagutinfedya@gmail.com>
2026-02-26 09:46:00 +0300

    feat: init

---
+++ java-sdk/build.gradle.kts
@@ -0,0 +1,39 @@
+import java.util.Properties
+
+plugins {
+    id("java")
+}
+
+fun getVersionFromBuildNumber(): String {
+    val buildNumberFile = file("build.number")
+    if (!buildNumberFile.exists()) {
+        return "0.0.1"
+    }
+
+    val props = Properties()
+    buildNumberFile.inputStream().use { props.load(it) }
+
+    val mainVersion = props.getProperty("mainVersion", "0")
+    val majorVersion = props.getProperty("majorVersion", "0")
+    val minorVersion = props.getProperty("minorVersion", "1")
+
+    return "${mainVersion}.${majorVersion}.${minorVersion}"
+}
+
+group = "com.fyordo.cms.sdk"
+version = getVersionFromBuildNumber()
+description = "Java SDK for CMS Project"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
