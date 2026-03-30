2db404f43658b66dd723ba2391f72c34f23b3e1f
Fedor <lagutinfedya@gmail.com>
2026-03-30 23:30:08 +0300

    fix: agent cancel

--- server/src/main/kotlin/com/fyordo/cms/server/config/props/AgentProperties.kt
+++
@@ -1,8 +0,0 @@
-package com.fyordo.cms.server.config.props
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "app.server.agent")
-data class AgentProperties(
-    val agentHealthcheckPeriodMs: Long = 1_000L
-)
